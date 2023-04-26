321b760248e68fc2fbf8be4f6465132f2ffc4ec6
Eduardo Menezes Rocha <eduardomenezesr@gmail.com>
2023-04-26 09:26:42 -0300

    change separators and add alt-substitute plugin

--- init.lua
+++ init.lua
@@ -58,6 +58,17 @@ return {
       },
     },
   },
+
+  heirline = {
+    separators = {
+      none = { "", "" },
+      left = { "", " " },
+      right = { " ", "" },
+      center = { " ", " " },
+      tab = { "", "" },
+    },
+  },
+
   -- This function is run last and is a good place to configuring
   -- augroups/autocommands and custom filetypes also this just pure lua so
   -- anything that doesn't fit in the normal config locations above can go here
